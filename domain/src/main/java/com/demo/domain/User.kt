5f6082a021fd51e11eb091278068adeadbc9912c
Rohit More <morerohit.more@gmail.com>
2025-12-03 14:43:13 +0530

    added multi modules.

---
+++ domain/src/main/java/com/demo/domain/User.kt
@@ -0,0 +1,8 @@
+package com.demo.domain
+
+data class User(
+    var id: Int ? = 0,
+    var name: String? = null,
+    var username: String? = null,
+    var email: String? = null,
+)
---
+++ ui/src/main/java/com/demo/mvvm/LauncherActivityV2.kt
@@ -0,0 +1,50 @@
+package com.demo.mvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.demo.domain.User
+import com.demo.mvvm.ui.theme.MVVMDemoTheme
+
+class LauncherActivityV2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MVVMDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var user = User()
+    user.email = "dummy@gmail.com"
+    Text(
+        text = "Hello $name! ${user.email}",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MVVMDemoTheme {
+        Greeting("Android")
+    }
+}
